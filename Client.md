571a6f83f085c403c6432c74137b63f8ca62a3d0
Bui Quang <zzzquangzzzthuzzz@gmail.com>
2022-04-06 23:56:35 +0700

    Update ReactJs

---
+++ Client.md
@@ -0,0 +1,6 @@
+# Client Sử Dụng React Js
+1 Babel chạy sử dụng
+- babel-core : chuyển đổi ES6 => ES5
+- babel-loader : cho phép cái file JS sử dụng được babel  và web-pack
+- babel-preset-env : cài đặt sẵn giúp bạn sử dụng Js nhiều môi trường khác nhau 
+- babel-preset-react : Chuyển đối JSX về Js
---
+++ Client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            <h1>Học react</h1>
+            <h2>Thanh Thanh 221</h2>
+            <h3>Tạ Phương Yến</h3>
+        </div>
+    );
+}
+// Đẩy vào thẻ có id là root
+ReactDOM.render(<App />,document.getElementById("root"));
